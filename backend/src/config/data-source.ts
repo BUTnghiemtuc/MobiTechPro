a3c6045849afe3fcf852473ddedbc0d11670b87b
T1Winter-daisuki <mocchaneo@gmail.com>
2026-03-18 22:24:23 +0700

    update connect to .env

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -5,24 +5,30 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD,
+    // làm local thì tắt các cmt ở đây đi
+    // type: "postgres",
+    // host: process.env.DB_HOST,
+    // port: parseInt(process.env.DB_PORT || "5432"),
+    // username: process.env.DB_USERNAME, 
+    // password: process.env.DB_PASSWORD,
     
-    // 1. Đổi DB_NAME thành DB_DATABASE cho khớp với file .env
-    database: process.env.DB_DATABASE,
+    // // 1. Đổi DB_NAME thành DB_DATABASE cho khớp với file .env
+    // database: process.env.DB_DATABASE,
     
     synchronize: true, 
     logging: false,
     
-    // 2. Ép bật SSL luôn, bỏ cái điều kiện check môi trường đi
-    ssl: { rejectUnauthorized: false },
-    
+    // làm local thì bật cmt 2 dòng này
+    type: "postgres",
+    url: process.env.DB_URL,
+
+    // làm local thì tắt cmt d1, bật d2
+    // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, 
+    ssl: process.env.SSL === 'true' ? { rejectUnauthorized: false } : false,
+
     // nếu có bất kì thực thể nào mới thì tự động được khai báo và dùng
     entities: [__dirname + "/../modules/**/1models/*.entity.{js,ts}"],
     
     subscribers: [],
     migrations: [],
-});
+}); 
